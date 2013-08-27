1b975a2ebd15ba272b04f1095c526d97175d238c
Stephan Smith <stephan.smith.bc93@gmail.com>
2013-08-27 17:23:40 -0400

    Added another test

--- routes/heroku.js
+++ routes/heroku.js
@@ -297,10 +297,10 @@ var cloneFetchGITRepo = function(gitUri, gitDir, cb) {
   } else {
     console.log('Cloning the Repo ' + gitDir);
 
-    var cmd9 = 'git clone ' + gitUri + '.git ' + gitDir + ';' + 
+    var cmd = 'git clone ' + gitUri + '.git ' + gitDir + ';' + 
                'ls ' + gitDir + ' -all;'
 
-    console.log('what is this:', cmd9);
+    console.log('what is this:', cmd);
 
     var cmd9 = 'git clone https://npr-ci:25thomson@github.com/nprds/composerAPI.git /app/tmp/composerapi;' +
               'ls /app/tmp/composerapi -all;'
